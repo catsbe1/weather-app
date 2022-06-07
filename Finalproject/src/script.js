8dde4a8f82c1c29cab1c4f797931a49f4b8153b7
catsbe1 <anaribeiro199814@gmail.com>
2022-06-07 02:05:42 +0100

    Connected the icon to the correspondent weather

--- Finalproject/src/script.js
+++ Finalproject/src/script.js
@@ -75,6 +75,11 @@ function changeTemp(response) {
   description.innerHTML = `${newDescription}`;
   let location = document.querySelector("#location");
   location.innerHTML = response.data.name;
+  let icon = document.querySelector(".icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function showPosition(position) {
